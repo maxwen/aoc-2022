660cba3f4127cd90b99097dd414a7bcb51a0cd95
maxwen <max.weninger@gmail.com>
2025-01-06 23:27:36 +0100

    day1 - part2 - just a warmup

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -11,6 +11,29 @@ pub fn read_lines_as_vec(filepath: &str) -> io::Result<Vec<String>> {
     Ok(lines.flatten().collect())
 }
 
+fn part2(lines: &[String]) -> u32 {
+    // 204610
+    let mut sum = 0u32;
+    let mut sum_list = vec![];
+
+    for (_, line) in lines.iter().enumerate() {
+        if !line.is_empty() {
+            let cal:u32 = line.parse().unwrap();
+            sum += cal
+        } else {
+            // println!("{} {}", elve, sum);
+            sum_list.push(sum);
+            sum = 0;
+        }
+    }
+    // println!("{} {}", elve, sum);
+    sum_list.push(sum);
+
+    sum_list.sort();
+    sum_list.reverse();
+    sum_list[0..3].into_iter().sum::<u32>()
+}
+
 fn part1(lines: &[String]) -> u32 {
     // 70374
     let mut sum = 0u32;
@@ -51,5 +74,7 @@ fn main() {
     //                  "9000",
     //                  "",
     //                  "10000"].iter().map(|s| s.to_string()).collect::<Vec<_>>();
-    println!("{}", part1(&lines))
+    println!("{}", part1(&lines));
+    println!("{}", part2(&lines));
+
 }
