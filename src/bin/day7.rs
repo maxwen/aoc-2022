13f4db3dd8b750c54bddedccedfb2b1263d00599
maxwen <max.weninger@gmail.com>
2025-01-07 17:33:06 +0100

    day7 - part2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,10 +1,5 @@
+use std::collections::{HashSet};
 use aoc_2022::read_lines_as_vec;
-
-fn part2(lines: &[String]) -> u32 {
-    let mut sum = 0u32;
-
-    sum
-}
 
 #[derive(Debug)]
 struct File {
@@ -58,29 +53,25 @@ fn calc_size(dir: &Dir, size: u32) -> u32 {
 }
 
 fn print_tree(dir: &Dir, level: usize) {
-    if (level == 0) {
+    if level == 0 {
         println!("- / (dir)")
     }
     for (_, d) in dir.dirs.iter().enumerate() {
-        println!("{:indent$}{number} - {dir} (dir)", number = " ", indent = level + 1, dir = d.name);
+        println!("{number:indent$}{number} - {dir} (dir)", number = " ", indent = level + 1, dir = d.name);
         print_tree(d, level + 1)
     }
     for (_, f) in dir.files.iter().enumerate() {
-        println!("{:indent$}{number} - {file} (file, {size})", number = " ", indent = level + 2, file = f.name, size = f.size);
+        println!("{number:indent$}{number} - {file} (file, {size})", number = " ", indent = level + 2, file = f.name, size = f.size);
     }
 }
 
 fn cwd_to_dir<'a>(root: &'a mut Dir, current_dir: &'a mut Dir, cwd: &'a Vec<&'a str>, level: usize) -> &'a mut Dir {
-    // if (cwd.len() == 0) {
-    //     return current_dir;
-    // }
-    // println!("{:?}", cwd);
     if level == cwd.len() - 1 {
         return current_dir;
     }
-    let current = cwd.get(level + 1).unwrap();
+    let next = cwd.get(level + 1).unwrap();
     for (_, d) in current_dir.dirs.iter_mut().enumerate() {
-        if (d.name == current.to_string()) {
+        if d.name == next.to_string() {
             return cwd_to_dir(root, d, cwd, level + 1);
         }
     }
@@ -94,77 +85,126 @@ fn part1(lines: &[String]) -> u32 {
 
     let mut root = Dir::new("/");
     let mut empty_root = Dir::new("/");
-    let mut dir_list = vec![];
+    let mut dir_list = HashSet::new();
     let mut cwd = vec!["/"];
 
     for (_, line) in lines.iter().enumerate() {
-        if line.starts_with("$") {
-            if line.starts_with("$ ls") {
-                // println!("{:?}", cwd);
-            } else if line.contains("$ cd") {
-                let dir = line.split_whitespace().last().unwrap();
-                match dir {
-                    ".." => {
-                        cwd.pop();
-                    }
-                    "/" => {
-                        cwd.clear();
-                        cwd.push("/");
-                    }
-                    _ => {
-                        cwd.push(dir);
-                        dir_list.push(cwd.clone());
-                    }
+        if line.starts_with("$ ls") {
+            // println!("{:?}", cwd);
+        } else if line.contains("$ cd") {
+            let dir = line.split_whitespace().last().unwrap();
+            match dir {
+                ".." => {
+                    cwd.pop();
+                }
+                "/" => {
+                    cwd.clear();
+                    cwd.push("/");
+                }
+                _ => {
+                    cwd.push(dir);
+                    dir_list.insert(cwd.clone());
                 }
             }
         } else {
-            let mut current_dir: &mut Dir = cwd_to_dir(&mut empty_root, &mut root, &cwd, 0);
+            let current_dir: &mut Dir = cwd_to_dir(&mut empty_root, &mut root, &cwd, 0);
 
             let s = line.split_whitespace().collect::<Vec<_>>();
             if s.first().unwrap().to_string() == "dir" {
                 let dir = s.last().unwrap();
                 current_dir.add_dir(Dir::new(dir))
             } else {
-                let size: u32 = s.first().unwrap().parse().unwrap();
-                let file = s.last().unwrap();
-                current_dir.add_file(File::new(size, file))
+                let size: u32 = s.first().unwrap().parse().unwrap_or(0);
+                let file: &str = s.last().unwrap_or(&" ");
+                if size != 0 {
+                    current_dir.add_file(File::new(size, file))
+                }
             }
         }
     }
 
-    // print_tree(&root, 0);
-
-    // let mut c = vec![];
-    // c.push("/");
-    // c.push("a");
-    // c.push("e");
-    // let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
-    // println!("{}", calc_size(e ,0));
-    //
-    // c.pop();
-    // let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
-    // println!("{}", calc_size(e ,0));
-    //
-    // c.pop();
-    // c.push("d");
-    // let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
-    // println!("{}", calc_size(e ,0));
-    //
-    // c.pop();
-    // let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
-    // println!("{}", calc_size(e ,0));
-    //
-    // println!("{:?}", dir_list);
-
     for c in dir_list.iter() {
         let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
         let size = calc_size(e, 0);
-        if (size <= 100000) {
+        if size <= 100000 {
             sum += size
         }
     }
     sum
 }
+
+
+fn part2(lines: &[String]) -> u32 {
+    // 13210366
+    let mut sum = 0u32;
+
+    let mut root = Dir::new("/");
+    let mut empty_root = Dir::new("/");
+    let mut dir_list = HashSet::new();
+    let mut cwd = vec!["/"];
+    dir_list.insert(cwd.clone());
+
+    for (_, line) in lines.iter().enumerate() {
+        if line.starts_with("$ ls") {
+            // println!("{:?}", cwd);
+        } else if line.contains("$ cd") {
+            let dir = line.split_whitespace().last().unwrap();
+            match dir {
+                ".." => {
+                    cwd.pop();
+                }
+                "/" => {
+                    cwd.clear();
+                    cwd.push("/");
+                }
+                _ => {
+                    cwd.push(dir);
+                    dir_list.insert(cwd.clone());
+                }
+            }
+        } else {
+            let current_dir: &mut Dir = cwd_to_dir(&mut empty_root, &mut root, &cwd, 0);
+
+            let s = line.split_whitespace().collect::<Vec<_>>();
+            if s.first().unwrap().to_string() == "dir" {
+                let dir = s.last().unwrap();
+                current_dir.add_dir(Dir::new(dir))
+            } else {
+                let size: u32 = s.first().unwrap().parse().unwrap_or(0);
+                let file: &str = s.last().unwrap_or(&" ");
+                if size != 0 {
+                    current_dir.add_file(File::new(size, file))
+                }
+            }
+        }
+    }
+
+    let mut c = vec![];
+    c.push("/");
+    let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
+
+    let total_size = 70000000;
+    let needed_free_size = 30000000;
+    let used_size = calc_size(e, 0);
+
+    let need_to_free_space =  needed_free_size - (total_size - used_size);
+    // println!("need_to_free_space = {}", need_to_free_space);
+
+    let mut possible_delete_size = vec![];
+    for c in dir_list.iter() {
+        let e = cwd_to_dir(&mut empty_root, &mut root, &c, 0);
+        let size = calc_size(e, 0);
+        if size > need_to_free_space {
+            possible_delete_size.push(size);
+        }
+    }
+    possible_delete_size.sort();
+    sum += *possible_delete_size.first().unwrap();
+
+    sum
+}
+
+
 fn main() {
     let lines = read_lines_as_vec("input/input_day7.txt").unwrap();
 
@@ -200,7 +240,7 @@ fn main() {
 }
 #[cfg(test)]
 mod tests {
-    use crate::part1;
+    use crate::{part1, part2};
 
     #[test]
     fn it_works() {
@@ -230,7 +270,7 @@ mod tests {
 
         let result = part1(&lines);
         assert_eq!(result, 95437);
-        // let result = part2(lines);
-        // assert_eq!(result, 19);
+        let result = part2(&lines);
+        assert_eq!(result, 24933642);
     }
 }
