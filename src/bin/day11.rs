31204d89be19944982c59396b1310a0e4bddb46a
maxwen <max.weninger@gmail.com>
2025-01-09 22:26:08 +0100

    day11 - part1 - learning the hard way

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,19 +1,19 @@
+use std::cell::RefCell;
 use aoc_2022::read_lines_as_vec;
-use std::collections::VecDeque;
-
+use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug, Clone)]
 struct Monkey {
-    stack: VecDeque<u32>,
-    operation: fn(u32) -> u32,
-    condition: fn(u32) -> bool,
+    stack: VecDeque<u64>,
+    operation: fn(u64) -> u64,
+    condition: fn(u64) -> bool,
     monkey_true_idx: usize,
     monkey_false_idx: usize,
-    inspect_count: u32,
+    inspect_count: u64,
 }
 
 impl Monkey {
-    fn add_item(&mut self, item: u32) {
+    fn add_item(&mut self, item: u64) {
         self.stack.push_back(item)
     }
 
@@ -22,6 +22,7 @@ impl Monkey {
             let item = self.stack.pop_front().unwrap();
             let item_new = (self.operation)(item);
             let item_bored = item_new / 3;
+            // let item_bored = item_new;
 
             if (self.condition)(item_bored) {
                 monkey_true.add_item(item_bored)
@@ -35,7 +36,10 @@ impl Monkey {
         false
     }
 }
-fn part1(lines: &[String]) -> u32 {
+fn part1(lines: &[String]) -> u64 {
+    // 66124
+    let mut monkey_map: HashMap<usize, RefCell<Monkey>> = HashMap::new();
+
     let mut monkey0 = Monkey {
         stack: VecDeque::new(),
         operation: |value| value * 13,
@@ -45,6 +49,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![75, 75, 98, 97, 79, 97, 64].iter().for_each(|x| monkey0.stack.push_back(*x));
+    monkey_map.insert(0, RefCell::new(monkey0));
 
     let mut monkey1 = Monkey {
         stack: VecDeque::new(),
@@ -55,6 +60,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![50, 99, 80, 84, 65, 95].iter().for_each(|x| monkey1.stack.push_back(*x));
+    monkey_map.insert(1, RefCell::new(monkey1));
 
     let mut monkey2 = Monkey {
         stack: VecDeque::new(),
@@ -65,6 +71,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![96, 74, 68, 96, 56, 71, 75, 53].iter().for_each(|x| monkey2.stack.push_back(*x));
+    monkey_map.insert(2, RefCell::new(monkey2));
 
     let mut monkey3 = Monkey {
         stack: VecDeque::new(),
@@ -75,6 +82,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![83, 96, 86, 58, 92].iter().for_each(|x| monkey3.stack.push_back(*x));
+    monkey_map.insert(3, RefCell::new(monkey3));
 
     let mut monkey4 = Monkey {
         stack: VecDeque::new(),
@@ -85,6 +93,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![99].iter().for_each(|x| monkey4.stack.push_back(*x));
+    monkey_map.insert(4, RefCell::new(monkey4));
 
     let mut monkey5 = Monkey {
         stack: VecDeque::new(),
@@ -95,6 +104,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![60, 54, 83].iter().for_each(|x| monkey5.stack.push_back(*x));
+    monkey_map.insert(5, RefCell::new(monkey5));
 
     let mut monkey6 = Monkey {
         stack: VecDeque::new(),
@@ -105,6 +115,7 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![77, 67].iter().for_each(|x| monkey6.stack.push_back(*x));
+    monkey_map.insert(6, RefCell::new(monkey6));
 
     let mut monkey7 = Monkey {
         stack: VecDeque::new(),
@@ -115,52 +126,26 @@ fn part1(lines: &[String]) -> u32 {
         inspect_count: 0,
     };
     vec![95, 65, 58, 76].iter().for_each(|x| monkey7.stack.push_back(*x));
+    monkey_map.insert(7, RefCell::new(monkey7));
 
     for _ in 0..20 {
-        {
-            while monkey0.inspect_item(&mut monkey2, &mut monkey7) {}
-        }
-        {
-            while monkey1.inspect_item(&mut monkey4, &mut monkey5) {}
-        }
-        {
-            while monkey2.inspect_item(&mut monkey7, &mut monkey3) {}
-        }
-        {
-            while monkey3.inspect_item(&mut monkey6, &mut monkey1) {}
-        }
-        {
-            while monkey4.inspect_item(&mut monkey0, &mut monkey5) {}
-        }
-        {
-            while monkey5.inspect_item(&mut monkey2, &mut monkey0) {}
-        }
-        {
-            while monkey6.inspect_item(&mut monkey4, &mut monkey1) {}
-        }
-        {
-            while monkey7.inspect_item(&mut monkey3, &mut monkey6) {}
+        for idx in 0..8 {
+            unsafe {
+                let m = monkey_map.get(&idx).unwrap();
+                let m_true = monkey_map.get(&m.borrow().monkey_true_idx).unwrap();
+                let m_false = monkey_map.get(&m.borrow().monkey_false_idx).unwrap();
+
+                while m.borrow_mut().inspect_item(&mut *m_true.borrow_mut(), &mut *m_false.borrow_mut()) {}
+            }
         }
     }
 
-    // println!("Monkey 0: {:?}", monkey0.stack);
-    // println!("Monkey 1: {:?}", monkey1.stack);
-    // println!("Monkey 2: {:?}", monkey2.stack);
-    // println!("Monkey 3: {:?}", monkey3.stack);
-
     let mut inspect_count = vec![];
-    inspect_count.push(monkey0.inspect_count);
-    inspect_count.push(monkey1.inspect_count);
-    inspect_count.push(monkey2.inspect_count);
-    inspect_count.push(monkey3.inspect_count);
-    inspect_count.push(monkey4.inspect_count);
-    inspect_count.push(monkey5.inspect_count);
-    inspect_count.push(monkey6.inspect_count);
-    inspect_count.push(monkey7.inspect_count);
+    monkey_map.values().for_each(|m| inspect_count.push(m.borrow().inspect_count));
 
     inspect_count.sort();
     inspect_count.reverse();
-    inspect_count[0] * inspect_count[1]
+    inspect_count.get(0..2).unwrap().iter().product::<u64>()
 }
 
 fn part2(lines: &[String]) -> u32 {
@@ -169,14 +154,12 @@ fn part2(lines: &[String]) -> u32 {
 
 
 fn main() {
-    // let lines = read_lines_as_vec("input/input_day11.txt").unwrap();
+    let lines = read_lines_as_vec("input/input_day11.txt").unwrap();
 
-    unsafe {
-        let lines = read_lines_as_vec("input_test/input_day11_test.txt").unwrap();
+    // let lines = read_lines_as_vec("input_test/input_day11_test.txt").unwrap();
 
-        println!("{}", part1(&lines));
-        println!("{}", part2(&lines));
-    }
+    println!("{}", part1(&lines));
+    println!("{}", part2(&lines));
 }
 #[cfg(test)]
 mod tests {
