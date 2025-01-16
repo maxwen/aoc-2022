ec929efa6dfd6c0448f0030756fcefc89ccb512d
maxwen <max.weninger@gmail.com>
2025-01-16 22:19:50 +0100

    day18 - part2 - there MUST be a better way but I dont find it yet

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,8 +1,8 @@
+use aoc_2022::read_lines_as_vec;
 use itertools::Itertools;
 use std::cmp::max;
 use std::collections::HashSet;
 use std::ops::Add;
-use aoc_2022::read_lines_as_vec;
 
 // thx to https://gitlab.com/harudagondi/alg-grid/-/blob/82a4951f244f8fb86f550970d1807971f8477e4c/src/three_dim.rs
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
@@ -86,27 +86,19 @@ fn part1(lines: &[String]) -> u32 {
     open_side as u32
 }
 
-fn get_enclosed_cubes(cubes: &HashSet<Point3D>, size: i32) -> HashSet<Point3D> {
+fn get_enclosed_cubes(cubes: &HashSet<Point3D>, max_x: i32, max_y: i32, max_z: i32) -> HashSet<Point3D> {
     let mut enclosed_cubes = HashSet::new();
-    for x in 0..size {
-        for y in 0..size {
-            for z in 0..size {
+    for z in 0..max_z + 1 {
+        for y in 0..max_y + 1 {
+            for x in 0..max_x + 1 {
                 let c = Point3D {
                     x,
                     y,
                     z,
                 };
                 // must be air
-                if !cubes.contains(&c) {
-                    let n = get_neighbors(c);
-                    let mut enclosed = true;
-                    for cn in n {
-                        if !cubes.contains(&cn) {
-                            enclosed = false;
-                            break;
-                        }
-                    }
-                    if enclosed {
+                if !cubes.contains(&c) && !enclosed_cubes.contains(&c) {
+                    if is_enclosed_air(cubes, &c, max_x, max_y, max_z) {
                         enclosed_cubes.insert(c);
                     }
                 }
@@ -116,6 +108,127 @@ fn get_enclosed_cubes(cubes: &HashSet<Point3D>, size: i32) -> HashSet<Point3D> {
     enclosed_cubes
 }
 
+fn is_enclosed_air(cubes: &HashSet<Point3D>, cube: &Point3D, max_x: i32, max_y: i32, max_z: i32) -> bool {
+    let this_x = cube.x;
+
+    let mut enclosed_left = false;
+    let mut enclosed_right = false;
+
+    for x in (0..this_x).rev() {
+        let c = Point3D {
+            x,
+            y: cube.y,
+            z: cube.z,
+        };
+        if cubes.contains(&c) {
+            enclosed_left = true;
+            break;
+        }
+    }
+    for x in this_x..max_x + 1 {
+        let c = Point3D {
+            x,
+            y: cube.y,
+            z: cube.z,
+        };
+        if cubes.contains(&c) {
+            enclosed_right = true;
+            break;
+        }
+    }
+    if !(enclosed_left && enclosed_right) {
+        return false;
+    }
+
+    let this_y = cube.y;
+
+    let mut enclosed_left = false;
+    let mut enclosed_right = false;
+
+    for y in (0..this_y).rev() {
+        let c = Point3D {
+            x: cube.x,
+            y,
+            z: cube.z,
+        };
+        if cubes.contains(&c) {
+            enclosed_left = true;
+            break;
+        }
+    }
+    for y in this_y..max_y + 1 {
+        let c = Point3D {
+            x: cube.x,
+            y,
+            z: cube.z,
+        };
+        if cubes.contains(&c) {
+            enclosed_right = true;
+            break;
+        }
+    }
+    if !(enclosed_left && enclosed_right) {
+        return false;
+    }
+
+    let this_z = cube.z;
+
+    let mut enclosed_left = false;
+    let mut enclosed_right = false;
+
+    for z in (0..this_z).rev() {
+        let c = Point3D {
+            x: cube.x,
+            y: cube.y,
+            z,
+        };
+        if cubes.contains(&c) {
+            enclosed_left = true;
+            break;
+        }
+    }
+    for z in this_z..max_z + 1 {
+        let c = Point3D {
+            x: cube.x,
+            y: cube.y,
+            z,
+        };
+        if cubes.contains(&c) {
+            enclosed_right = true;
+            break;
+        }
+    }
+    if !(enclosed_left && enclosed_right) {
+        return false;
+    }
+
+    true
+}
+
+#[warn(dead_code)]
+fn print_cube(cubes: &HashSet<Point3D>, enclosed_cubes: &HashSet<Point3D>, max_x: i32, max_y: i32, max_z: i32) {
+    for z in 0..max_z + 1 {
+        println!("{}", z);
+        for y in 0..max_y + 1 {
+            for x in 0..max_x + 1 {
+                let c = Point3D {
+                    x,
+                    y,
+                    z,
+                };
+                if enclosed_cubes.contains(&c) {
+                    print!("@")
+                } else if cubes.contains(&c) {
+                    print!("#")
+                } else {
+                    print!(".")
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+}
 fn get_open_sides(cubes: &HashSet<Point3D>) -> usize {
     let mut open_side = cubes.len() * 6;
 
@@ -130,8 +243,20 @@ fn get_open_sides(cubes: &HashSet<Point3D>) -> usize {
     open_side
 }
 
+fn filter_wrong_enclosed(cubes: &HashSet<Point3D>, enclosed_cubes: &HashSet<Point3D>) -> HashSet<Point3D> {
+    let mut false_positives = HashSet::new();
+    for &e in enclosed_cubes {
+        if !get_neighbors(e)
+            .iter()
+            .all(|&x| cubes.contains(&x) || enclosed_cubes.contains(&x)) {
+            false_positives.insert(e);
+        }
+    }
+    false_positives
+}
+
 fn part2(lines: &[String]) -> u32 {
-    // < 3330
+    // 2074
     let mut cubes = HashSet::new();
     let mut max_x = 0;
     let mut max_y = 0;
@@ -156,12 +281,23 @@ fn part2(lines: &[String]) -> u32 {
         };
         cubes.insert(cube);
     }
-    let enclosed_cubes = get_enclosed_cubes(&cubes, max_x);
-    println!("{:?}", enclosed_cubes);
 
-    for c in enclosed_cubes {
-        cubes.insert(c);
+    let mut enclosed_cubes = get_enclosed_cubes(&cubes, max_x, max_y, max_z);
+    // println!("enclosed_cubes {}", enclosed_cubes.len());
+
+    // print_cube(&cubes, &enclosed_cubes, max_x, max_y, max_z);
+
+    let mut false_positives = filter_wrong_enclosed(&cubes, &enclosed_cubes);
+    while false_positives.len() != 0 {
+        // println!("false_positives {}", false_positives.len());
+
+        enclosed_cubes.retain(|&c| !false_positives.contains(&c));
+        false_positives = filter_wrong_enclosed(&cubes, &enclosed_cubes);
     }
+
+    // print_cube(&cubes, &final_enclosed, max_x, max_y, max_z);
+
+    cubes.extend(&enclosed_cubes);
 
     let open_side = get_open_sides(&cubes);
     open_side as u32
@@ -185,7 +321,7 @@ fn main() {
     //                  "2,3,5"].iter().map(|s| s.to_string()).collect::<Vec<_>>();
 
     println!("{}", part1(&lines));
-    // println!("{}", part2(&lines));
+    println!("{}", part2(&lines));
 }
 
 #[cfg(test)]
